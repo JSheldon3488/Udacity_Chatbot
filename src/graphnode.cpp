f4d977b67f0a28760656ba9c373c91c98bdca429
Justin Sheldon <JSheldon3488@Gmail.com>
2020-02-26 21:25:16 +0000

    Completed Task 0: Find Bug

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    //delete _chatBot; (This is a bug. No call to new, should not have a call to delete)
 
     ////
     //// EOF STUDENT CODE
